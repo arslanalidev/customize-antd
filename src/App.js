7ba159742356b054e99eab61b1fa6f1273aade11
Arslan Ali <arslanalidev@gmail.com>
2021-03-08 15:35:18 +0500

    customized antd solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+import "./App.less";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Arslan</Button>
+    </div>
+  );
+}
+
+export default App;
